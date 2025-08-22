516a6407abed8a877890e660b230ea5eb7140eb3
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2025-08-22 17:50:13 +0530

    Added <ElectricBorder /> Animation

--- src/constants/Categories.ts
+++ src/constants/Categories.ts
@@ -1,5 +1,5 @@
 // Highlighted sidebar items
-export const NEW = ['Target Cursor', 'Ripple Grid', 'Magic Bento', 'Galaxy', 'Text Type', 'Glass Surface', 'Sticker Peel', 'Scroll Stack', 'Faulty Terminal', 'Pill Nav', 'Card Nav', 'Logo Loop'];
+export const NEW = ['Target Cursor', 'Ripple Grid', 'Magic Bento', 'Galaxy', 'Text Type', 'Glass Surface', 'Sticker Peel', 'Scroll Stack', 'Faulty Terminal', 'Pill Nav', 'Card Nav', 'Logo Loop', 'Electric Border'];
 export const UPDATED = [];
 
 // Used for main sidebar navigation
@@ -41,6 +41,7 @@ export const CATEGORIES = [
       'Logo Loop',
       'Pixel Transition',
       'Target Cursor',
+      'Electric Border',
       'Sticker Peel',
       'Ribbons',
       'Glare Hover',
